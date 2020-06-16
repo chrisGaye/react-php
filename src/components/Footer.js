07ba227b875f49370b8a009376ff17306b4f3d5b
chrisGaye <gayetalib90@gmail.com>
2020-06-16 19:25:07 +0000

    start react and php

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="row" id="footer">
+                <div className="medium-12 columns">
+                     <p>Copyright 2019 , <a href="https://fahmidasclassroom.com"> fahmidasclassroom</a></p>
+                </div>
+            </div>
+            );
+        }
+    }
+export default Footer;
